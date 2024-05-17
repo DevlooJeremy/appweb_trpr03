3f074fdddecb575a5ee77f8946339d28d47cfbd4
GlucoseLeGlock <minigo17@outlook.com>
2024-05-17 15:00:36 -0400

    add student and add category form validation done

--- src/components/TeacherComponent/AddCategoryForm.vue
+++ src/components/TeacherComponent/AddCategoryForm.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { useCategoryStore } from "../../stores/categoryStore";
 
 const emit = defineEmits<{
@@ -9,13 +10,32 @@ const categoryStore = useCategoryStore()
 
 const name: any = defineModel("name");
 
-function addUser() {
-    categoryStore.addCategory(name.value)
-    close()
+const hasError = ref<boolean>(false)
+const nameErrorShown = ref<boolean>(false)
+const nameErrorMessage = ref<string>("")
+
+function addCategory() {
+    hasError.value = false
+    validateName()
+    if (!hasError.value) {
+        categoryStore.addCategory(name.value)
+        close()
+    }
 }
 
 function close() {
     emit("close")
+}
+
+function validateName(){
+    if ((name.value??"").trim() == "") {
+        hasError.value = true
+        nameErrorShown.value = true
+        nameErrorMessage.value = "Le nom est requis et ne peut pas être vide."
+    }
+    else {
+        nameErrorShown.value = false
+    }
 }
 
 </script>
@@ -24,8 +44,9 @@ function close() {
     <form class="d-flex flex-column bg-white border border-primary border-2 rounded p-2" action="">
         <h1 class="text-center">Ajouter une catégorie</h1>
         <input class="m-1" type="text" v-model="name" placeholder="Nom de la catégorie">
+        <div v-if="nameErrorShown" class="mx-1 bg-danger text-white text-center rounded">{{ nameErrorMessage }}</div>
         <div class="ms-auto">
-            <div class="btn btn-outline-dark border-2 fw-bold m-2" @click="addUser">Ajouter</div>
+            <div class="btn btn-outline-dark border-2 fw-bold m-2" @click="addCategory">Ajouter</div>
             <div class="btn btn-outline-dark border-2 fw-bold m-2" @click="close">Annuler</div>
         </div>
     </form>
--- src/components/TeacherComponent/AddStudentForm.vue
+++ src/components/TeacherComponent/AddStudentForm.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { useUserStore } from "../../stores/userStore";
 
 const emit = defineEmits<{
@@ -11,13 +12,71 @@ const completeName: any = defineModel("completeName");
 const password: any = defineModel("password");
 const email: any = defineModel("email");
 
+const hasError = ref<boolean>(false)
+const nameErrorShown = ref<boolean>(false)
+const nameErrorMessage = ref<string>("")
+const passwordErrorShown = ref<boolean>(false)
+const passwordErrorMessage = ref<string>("")
+const emailErrorShown = ref<boolean>(false)
+const emailErrorMessage = ref<string>("")
+const EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
+
 function addUser() {
-    userStore.addStudent(completeName.value, password.value, email.value)
-    close()
+    hasError.value = false
+    validateName()
+    validatePassword()
+    validateEmail()
+    if (!hasError.value) {
+        userStore.addStudent(completeName.value, password.value, email.value)
+        close()
+    }
 }
 
 function close() {
     emit("close")
+}
+
+function validateName(){
+    if ((completeName.value??"").trim() == "") {
+        hasError.value = true
+        nameErrorShown.value = true
+        nameErrorMessage.value = "Le nom est requis et ne peut pas être vide."
+    }
+    else {
+        nameErrorShown.value = false
+    }
+}
+
+function validatePassword(){
+    if ((password.value??"").trim() == "") {
+        hasError.value = true
+        passwordErrorShown.value = true
+        passwordErrorMessage.value = "Le mot de passe est requis et ne peut pas être vide."
+    }
+    else if(password.value.length < 4) {
+        hasError.value = true
+        passwordErrorShown.value = true
+        passwordErrorMessage.value = "Le mot de passe doit faire 4+ charactères de long."
+    }
+    else {
+        passwordErrorShown.value = false
+    }
+}
+
+function validateEmail(){
+    if ((email.value??"").trim() == "") {
+        hasError.value = true
+        emailErrorShown.value = true
+        emailErrorMessage.value = "L\'adresse courriel est requis et ne peut pas être vide."
+    }
+    else if(!email.value.match(EMAIL_REGEX)) {
+        hasError.value = true
+        emailErrorShown.value = true
+        emailErrorMessage.value = "L\'adresse courriel doit avoir un format valide."
+    }
+    else {
+        emailErrorShown.value = false
+    }
 }
 
 </script>
@@ -26,8 +85,11 @@ function close() {
     <form class="d-flex flex-column bg-white border border-primary border-2 rounded p-2" action="">
         <h1 class="text-center">Ajouter un élève</h1>
         <input class="m-1" type="text" v-model="completeName" placeholder="Nom complet">
+        <div v-if="nameErrorShown" class="mx-1 px-1 bg-danger text-white text-center rounded">{{ nameErrorMessage }}</div>
         <input class="m-1" type="text" v-model="password" placeholder="Mot de passe">
+        <div v-if="passwordErrorShown" class="mx-1 px-1 bg-danger text-white text-center rounded">{{ passwordErrorMessage }}</div>
         <input class="m-1" type="email" v-model="email" placeholder="Adresse courriel">
+        <div v-if="emailErrorShown" class="mx-1 px-1 bg-danger text-white text-center rounded">{{ emailErrorMessage }}</div>
         <div class="ms-auto">
             <div class="btn btn-outline-dark border-2 fw-bold m-2" @click="addUser">Ajouter</div>
             <div class="btn btn-outline-dark border-2 fw-bold m-2" @click="close">Annuler</div>
